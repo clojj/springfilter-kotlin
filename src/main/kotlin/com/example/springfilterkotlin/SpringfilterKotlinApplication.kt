2ad1a533410d05d0b96c92d489e27836d94268c4
clojj <clojj@github.com>
2023-09-09 19:24:35 +0200

    Initial commit

---
+++ src/main/kotlin/com/example/springfilterkotlin/SpringfilterKotlinApplication.kt
@@ -0,0 +1,11 @@
+package com.example.springfilterkotlin
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringfilterKotlinApplication
+
+fun main(args: Array<String>) {
+	runApplication<SpringfilterKotlinApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/springfilterkotlin/SpringfilterKotlinApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springfilterkotlin
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringfilterKotlinApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
